f43cb074e73b8a3fca8cdf662bc06ba7a3720403
breinero <bryan.reinero@10gen.com>
2015-04-22 17:33:26 -0700

    Removed circuit breaker features as it belongs in a separate project
    Moved to https://github.com/breinero/Firehose

--- src/com/mongodb/tour/circuitbreaker/CircuitBreakerMBean.java
+++
@@ -1,6 +0,0 @@
-package com.mongodb.tour.circuitbreaker;
-public interface CircuitBreakerMBean {
-	public String report();
-	public void setBreaker( String name, String type, Double threshold );
-	public void resetBreaker( String name );
-}
--- src/com/mongodb/tour/circuitbreaker/Threshold.java
+++
@@ -1,9 +0,0 @@
-package com.mongodb.tour.circuitbreaker;
-
-import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
-
-public interface Threshold {
-	public boolean isExceeded(DescriptiveStatistics stats);
-	public BreakerType getType();
-	public double getValue();
-}
